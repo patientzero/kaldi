e69790b4b56a5e906be08bc2d844b0b8ad52dbdd
Sebastian Bayerl <bayerl@sbmbp.local>
2018-06-05 11:31:12 +0200

    Add download of dicts for test and dev sets

--- egs/verbmobil/s5/local/vm_dic_download.sh
+++ egs/verbmobil/s5/local/vm_dic_download.sh
@@ -17,17 +17,38 @@ if [ ! -d $vm1_dir ] || [ ! -d $vm2_dir ]; then
   exit 1; 
 fi  
 
+# General source of lex and sets: ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/
 
 cd $vm1_dir
-echo " *** Downloading vm1 train dictionary***" 
+echo " *** Downloading vm1 train dictionary ***" 
 wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM1_TRAIN
 wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM1_TRAIN.lex
 wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM1_TRAIN.list
+echo " *** Downloading vm1 test dictionary ***" 
+wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM1_TEST
+wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM1_TEST.lex
+wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM1_TEST.list
+echo " *** Downloading vm1 dev dictionary ***" 
+wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM1_DEV
+wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM1_DEV.lex
+wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM1_DEV.list
+
+
 cd -
 
+
+
 cd $vm2_dir
-echo " *** Downloading vm2 train dictionary***" 
+echo " *** Downloading vm2 train dictionary ***" 
 wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM2_TRAIN
 wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM2_TRAIN.lex
 wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM2_TRAIN.list
+echo " *** Downloading vm2 test dictionary ***" 
+wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM2_TEST
+wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM2_TEST.lex
+wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM2_TEST.list
+echo " *** Downloading vm2 dev dictionary ***" 
+wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM2_DEV
+wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM2_DEV.lex
+wget ftp://ftp.bas.uni-muenchen.de/pub/BAS/VM/SETS/VM2_DEV.list
 cd -
