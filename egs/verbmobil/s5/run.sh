43191c58b2aaf9168d097fe3d5598b0df49facd9
Sebastian Bayerl <bayerl@sbmbp.dhcp.fh-rosenheim.de>
2018-06-07 16:56:58 +0200

    fix mixup

--- egs/verbmobil/s5/run.sh
+++ egs/verbmobil/s5/run.sh
@@ -48,8 +48,8 @@ steps/compute_cmvn_stats.sh data/test
 # 24425 data count in trainset, split 6k utterances for monophone training
 
 
-# utils/subset_data_dir.sh --shortest data/train 2000 data/train_2kshort # https://stackoverflow.com/questions/46202653/bash-error-in-sort-sort-write-failed-standard-output-broken-pipe
-utils/subset_data_dir.sh data/train --shortest 2000 data/train_2kshort
+# https://stackoverflow.com/questions/46202653/bash-error-in-sort-sort-write-failed-standard-output-broken-pipe
+utils/subset_data_dir.sh --shortest data/train 2000 data/train_2kshort
 utils/subset_data_dir.sh data/train 6000 data/train_6k
 utils/subset_data_dir.sh data/train 12000 data/train_half
 
